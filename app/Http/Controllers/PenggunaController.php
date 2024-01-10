9cde4768fc2ef165eb4f29ec73d34f0d35a0ab3a
Dimas Pangestu <pangestudimas719@gmail.com>
2024-01-10 20:59:50 +0700

    data user

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PenggunaController extends Controller
 {
     public function index()
     {
+        $pengguna = User::all();
+
         return view('pages.admin.pengguna.index', [
             'title' => 'Pengguna',
+            'pengguna' => $pengguna
         ]);
     }
 }
