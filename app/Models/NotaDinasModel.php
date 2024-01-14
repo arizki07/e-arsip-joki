509d53ccb0f3484b33f23dbda20c31728074fd51
Dimas Pangestu <pangestudimas719@gmail.com>
2024-01-14 16:21:52 +0700

    export word

---
+++ app/Models/NotaDinasModel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NotaDinasModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'nota_dinas';
+       protected $primaryKey = 'id_nota_dinas';
+       protected $fillable = [
+        'nd_kpa_id',
+        'nd_nama_kegiatan',
+        'nd_sub_kegiatan',
+        'nd_perihal',
+        'nd_nomor_nota',
+        'nd_uraian_kegiatan',
+        'nd_tanggal',
+        'nd_jumlah_biaya'
+    ];
+
+    public function kpa()
+    {
+        return $this->belongsTo(BiodataModel::class, 'nd_kpa_id', 'id_biodata');
+    }
+}
---
+++ resources/views/pages/admin/pengajuan/create.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.main')
+@section('content')
+    @include('component.alerts')
+
+    <div class="col-12 col-lg-12 col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="page-title">
+                    <div class="row">
+                        <div class="col-12 col-md-6 order-md-1 order-last">
+                            <h3>{{ $title }}</h3>
+                        </div>
+                        <div class="col-12 col-md-6 order-md-2 order-first">
+                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="{{ url('dashboard') }}">Dashboard</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">{{ $title }}</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="section">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">{{ $title }}</h4>
+            </div>
+
+            <div class="card-body">
+                <form action="{{ route('pengajuan.store') }}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="basicInput">Nama Kegiatan</label>
+                        <input type="text" class="form-control mt-1" name="nd_nama_kegiatan" id="basicInput"
+                            value="{{ old('nd_nama_kegiatan') }}" placeholder="Nama Kegiatan">
+                    </div>
+                    <div class="form-group">
+                        <label for="basicInput">Sub Kegiatan</label>
+                        <input type="text" class="form-control mt-1" name="nd_sub_kegiatan" id="basicInput"
+                            value="{{ old('nd_sub_kegiatan') }}" placeholder="Sub Kegiatan">
+                    </div>
+                    <div class="form-group">
+                        <label for="basicInput">Perihal</label>
+                        <input type="text" class="form-control mt-1" name="nd_perihal" id="basicInput"
+                            value="{{ old('nd_perihal') }}" placeholder="Perihal">
+                    </div>
+                    <div class="form-group">
+                        <label for="basicInput">Nomor Nota</label>
+                        <input type="text" class="form-control mt-1" name="nd_nomor_nota" id="basicInput"
+                            value="{{ old('nd_nomor_nota') }}" placeholder="Nomor Nota">
+                    </div>
+                    <div class="form-group">
+                        <label for="basicInput">Tanggal</label>
+                        <input type="date" class="form-control mt-1" name="nd_tanggal" id="basicInput"
+                            placeholder="Tanggal" value="{{ old('nd_tanggal') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="basicInput">Jumlah Biaya</label>
+                        <input type="text" class="form-control mt-1" name="nd_jumlah_biaya" id="basicInput"
+                            value="{{ old('nd_jumlah_biaya') }}" placeholder="Jumlah Biaya">
+                    </div>
+                    <div class="form-group">
+                        <label for="basicInput">Uraian Kegiatan</label>
+                        <textarea type="text" rows="3" class="form-control mt-1" name="nd_uraian_kegiatan" id="basicInput"
+                            placeholder="Uraian Kegiatan">{{ old('nd_uraian_kegiatan') }}</textarea>
+                    </div>
+                    <div class="col-sm-12 mt-4">
+                        <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
+                        <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                        <a type="button" href="{{ url('pengajuan') }}" class="btn btn-warning me-1 mb-1">Kembali</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection
