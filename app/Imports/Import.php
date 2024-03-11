157540bd69ad2dd12795c093ac61b74af1282511
Moch Syarif Hidayat <mochsyarifhidayat24@gmail.com>
2024-03-11 16:20:43 +0700

    --

--- app/Imports/Import.php
+++ app/Imports/Import.php
@@ -6,8 +6,6 @@ use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 use Maatwebsite\Excel\Facades\Excel;
-use App\Models\TestingModel;
-use App\Models\TestingTempatUraianModel;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
 class Import implements WithMultipleSheets
